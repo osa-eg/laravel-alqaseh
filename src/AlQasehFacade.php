912fcc3d9b99e84f919b9ca07f1cd92c40f72750
Osama Saad <osama.96.eg@gmail.com>
2025-04-29 23:23:42 +0300

    updates

--- src/AlQasehFacade.php
+++ src/AlQasehFacade.php
@@ -4,6 +4,15 @@ namespace Osama\AlQaseh;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Osama\AlQaseh\Responses\PaymentResponse createPayment(float $amount, string $currency, string $orderId, string $description, string $redirectUrl, ?string $transactionType = null, ?string $country = null, ?array $customData = null, ?string $email = null)
+ * @method static \Osama\AlQaseh\Responses\PaymentResponse getPaymentInfo(string $paymentId)
+ * @method static \Osama\AlQaseh\Responses\PaymentResponse getPaymentHistory(array $filters, int $limit = 50, string $orderBy = 'desc')
+ * @method static string downloadPaymentHistory(array $filters)
+ * @method static void retryPayment(string $paymentId, string $details)
+ * @method static void revokePayment(string $paymentId, string $details)
+ * @method static \Osama\AlQaseh\Responses\PaymentResponse processPayment(string $token, string $cardNumber, string $cvv, string $expiryMonth, string $expiryYear)
+ */
 class AlQasehFacade extends Facade
 {
     /**

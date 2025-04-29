d3def3a0b9004c46144f7a0456fd9bb901e88449
Osama Saad <osama.96.eg@gmail.com>
2025-04-30 00:28:13 +0300

    updates

--- src/AlQaseh.php
+++ src/AlQaseh.php
@@ -430,7 +430,7 @@ class AlQaseh
             if ($response->failed()) {
                 throw new AlQasehException(
                     $responseData['err'] ?? 'Unknown error occurred',
-                    $responseData['error_code'] ?? $response->status(),
+                    $response->status(),
                     $responseData['reference_code'] ?? null
                 );
             }
--- src/Exceptions/AlQasehException.php
+++ src/Exceptions/AlQasehException.php
@@ -6,5 +6,42 @@ use Exception;
 
 class AlQasehException extends Exception
 {
-    // Custom exception for Al Qaseh API errors
+    protected $errorCode;
+    protected $referenceCode;
+
+    /**
+     * AlQasehException constructor.
+     *
+     * @param string $message
+     * @param string $errorCode
+     * @param string $referenceCode
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $errorCode = "", $referenceCode = "", $code = 0)
+    {
+        parent::__construct($message, $code);
+        $this->errorCode = $errorCode;
+        $this->referenceCode = $referenceCode;
+    }
+
+    /**
+     * Get the error code.
+     *
+     * @return string
+     */
+    public function getErrorCode()
+    {
+        return $this->errorCode;
+    }
+
+    /**
+     * Get the reference code.
+     *
+     * @return string
+     */
+    public function getReferenceCode()
+    {
+        return $this->referenceCode;
+    }
 }
